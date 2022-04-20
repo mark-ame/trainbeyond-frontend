80529a008f6e40b512e61ab8694ad7d660a6975b
Charles Atienza <catienza@webcreek.com>
2022-04-20 21:05:07 +0800

    change apiURL and change script of develop

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,8 @@ module.exports = {
         {
             resolve: `gatsby-source-strapi`,
             options: {
-                apiURL: `http://localhost:1337`,
+                // apiURL: `http://localhost:1337`,
+                apiURL: `https://trainbeyond-backend.herokuapp.com`,
                 queryLimit: 1000,
                 contentTypes: [
                     "top-features",
